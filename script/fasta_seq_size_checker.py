af0729d795144c5729cf792deafb0696d8698a22
Dr Ahsan Z Rizvi <ahsan.rizvi@iar.ac.in>
2025-05-03 15:40:20 +0200

    Fasta Frequency checker

---
+++ script/fasta_seq_size_checker.py
@@ -0,0 +1,23 @@
+#!/opt/cray/pe/python/3.11.5/bin/python
+#- Dr Ahsan Z Rizvi
+#-------------------------
+import numpy
+import sys
+#-------------------------
+
+seq_len=list()
+with open(sys.argv[1]) as file:#Read file
+    for line in file:
+        temp=line[0]
+        if temp != '>':
+           seq_len.append(len(line)-1)
+
+
+nonRed,freq = numpy.unique(seq_len,return_counts=True)
+temp=sys.argv[1].split('/') 
+temp2=temp[-1]
+temp3=temp2.split('.')
+temp4=temp3[0]+','+str(freq[0])+','+str(freq[1])+','+str(freq[2])+','+str(freq[3])+','+str(freq[4])+','+str(freq[5])
+print(temp4)
+
+
